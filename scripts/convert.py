b3c53df6b36053c22bd5805f728f83204a0524f6
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2023-08-18 00:08:21 +0200

    added basic.rnbopat

--- scripts/convert.py
+++ scripts/convert.py
@@ -13,8 +13,8 @@ except ImportError:
 
 
 
-def convert_maxpat_to_yaml(maxpat_file):
-    path = pathlib.Path(maxpat_file)
+def convert_maxfile_to_yaml(max_file):
+    path = pathlib.Path(max_file)
     name = path.stem
     with open(path) as f:
         d = json.load(f)
@@ -24,25 +24,25 @@ def convert_maxpat_to_yaml(maxpat_file):
         yml = yaml.dump(d, Dumper=Dumper)
         f.write(yml)
 
-def convert_yaml_to_maxpat(yml_file):
+def convert_yaml_to_maxfile(yml_file, to_suffix='.maxpat'):
     path = pathlib.Path(yml_file)
     name  = path.stem
     with open(path) as f:
         d = yaml.load(f, Loader=Loader)
 
-    maxpat_file = path.parent / f'{name}.maxpat'
-    with open(maxpat_file, 'w') as f:
+    max_file = path.parent / f'{name}{to_suffix}'
+    with open(max_file, 'w') as f:
         _json = json.dumps(d, indent='\t')
         f.write(_json)
 
-def main(target):
+def main(target, to_suffix='.maxpat'):
     path = pathlib.Path(target)
     if path.suffix in ['.yml', '.yaml']:
-        print(f"converting {path} to .maxpat file:")
-        convert_yaml_to_maxpat(path)
-    elif path.suffix in ['.maxpat', '.maxhelp']:
+        print(f"converting {path} to max file:")
+        convert_yaml_to_maxfile(path, to_suffix)
+    elif path.suffix in ['.maxpat', '.maxhelp', '.rnbopat']:
         print(f"converting {path} to .yml file")
-        convert_maxpat_to_yaml(path)
+        convert_maxfile_to_yaml(path)
     else:
         print(f"cannot process: {path}")
 
@@ -50,10 +50,16 @@ def main(target):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-                    prog='maxpat-converter',
-                    description='converts maxpat to and from yml')
+                    prog='maxfile-converter',
+                    description='converts max file to and from yml')
     arg = option = parser.add_argument
-    arg("target", help="target maxpat or yml file to convert")
+    arg("target", help="target max file or yml file to convert")
+    option('--to-rnbopat', help="convert yaml to .rnbopat max file type", action="store_true")
+    option('--to-maxhelp', help="convert yaml to .maxhelp max file type", action="store_true")
     args = parser.parse_args()
-    # print(args.target)
-    main(args.target)
+    if args.to_rnbopat:
+        main(args.target, to_suffix='.rnbopat')
+    elif args.to_maxhelp:
+        main(args.target, to_suffix='.maxhelp')
+    else:
+        main(args.target)
